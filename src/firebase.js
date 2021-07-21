231e2202b5953c4d678e1f56046c2a5d62f2763c
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2021-07-22 02:44:52 +0530

    feat : add firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCUdZJbWFXv34M_vLN5gepiVR9tdEtvr8w",
+    authDomain: "slack-clone-react-941f5.firebaseapp.com",
+    projectId: "slack-clone-react-941f5",
+    storageBucket: "slack-clone-react-941f5.appspot.com",
+    messagingSenderId: "1053224341094",
+    appId: "1:1053224341094:web:455fb628b0e53bf96167a7",
+    measurementId: "G-YXFDCKPQH2"
+  };
